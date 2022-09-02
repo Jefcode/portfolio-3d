518b30d8f5ed2c57e9ff705155e2e5058a9bb24d
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2022-09-02 16:51:34 +0430

    fix the initail size of the canvas in Sizes.js

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -2,50 +2,50 @@ import { EventEmitter } from 'events';
 import Experience from '../Experience';
 
 export default class Sizes extends EventEmitter {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		this.experienceElement = document.querySelector('.experience');
-		let width = this.experienceElement.offsetWidth;
-		let height = this.experienceElement.offsetHeight;
+        this.experienceElement = document.querySelector('.experience');
+        let width = window.innerWidth;
+        let height = window.innerHeight;
 
-		this.experience = new Experience();
-		this.canvas = this.experience.canvas;
+        this.experience = new Experience();
+        this.canvas = this.experience.canvas;
 
-		// Setup
-		this.width = width;
-		this.height = height;
-		this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-		this.aspect = this.width / this.height;
-		this.frustum = 2;
+        // Setup
+        this.width = width;
+        this.height = height;
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+        this.aspect = this.width / this.height;
+        this.frustum = 2;
 
-		// Resize Event
-		window.addEventListener('resize', () => {
-			let newWidth = this.experienceElement.offsetWidth;
-			let newHeight = this.experienceElement.offsetHeight;
+        // Resize Event
+        window.addEventListener('resize', () => {
+            let newWidth = this.experienceElement.offsetWidth;
+            let newHeight = this.experienceElement.offsetHeight;
 
-			// if (newWidth === width && newHeight === height) {
-			// 	return;
-			// }
+            // if (newWidth === width && newHeight === height) {
+            // 	return;
+            // }
 
-			this.width = newWidth;
-			this.height = newHeight;
-			this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-			this.aspect = this.width / this.height;
+            this.width = newWidth;
+            this.height = newHeight;
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+            this.aspect = this.width / this.height;
 
-			this.emit('resize');
-		});
-	}
+            this.emit('resize');
+        });
+    }
 
-	destroy() {
-		// Remove Resize Event
-		window.removeEventListener('resize', () => {
-			(this.width = window.innerWidth),
-				(this.height = window.innerHeight),
-				(this.pixelRatio = Math.min(window.devicePixelRatio, 2));
-			console.log('resize happened');
+    destroy() {
+        // Remove Resize Event
+        window.removeEventListener('resize', () => {
+            (this.width = window.innerWidth),
+                (this.height = window.innerHeight),
+                (this.pixelRatio = Math.min(window.devicePixelRatio, 2));
+            console.log('resize happened');
 
-			this.emit('resize');
-		});
-	}
+            this.emit('resize');
+        });
+    }
 }
