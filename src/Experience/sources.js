3665742b6efb04400de329fcf6285bf92b2cfa42
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2022-11-17 13:06:44 +0330

    add mobliran project

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,52 +1,57 @@
 export default [
-	{
-		name: 'room',
-		type: 'gltfModel', // texture, gltfModel, cubeTexture
-		path: '/models/room-v3.glb',
-	},
-	{
-		name: 'bakedTextureDay',
-		type: 'texture',
-		path: '/textures/baked-day.jpg',
-	},
-	{
-		name: 'bakedTextureNatural',
-		type: 'texture',
-		path: '/textures/baked-natural.jpg',
-	},
-	{
-		name: 'bakedTextureNight',
-		type: 'texture',
-		path: '/textures/baked-night.jpg',
-	},
-	{
-		name: 'iphoneTexture',
-		type: 'texture',
-		path: '/textures/iPhone.jpg',
-	},
-	{
-		name: 'project_1',
-		type: 'texture',
-		path: '/textures/project_1.jpg',
-	},
-	{
-		name: 'project_2',
-		type: 'texture',
-		path: '/textures/project_2.jpg',
-	},
-	{
-		name: 'project_3',
-		type: 'texture',
-		path: '/textures/project_3.png',
-	},
-	{
-		name: 'projectDefault',
-		type: 'texture',
-		path: '/textures/project-default.jpg',
-	},
-	{
-		name: 'video',
-		type: 'videoTexture',
-		path: '/textures/video.mp4',
-	},
+  {
+    name: 'room',
+    type: 'gltfModel', // texture, gltfModel, cubeTexture
+    path: '/models/room-v3.glb',
+  },
+  {
+    name: 'bakedTextureDay',
+    type: 'texture',
+    path: '/textures/baked-day.jpg',
+  },
+  {
+    name: 'bakedTextureNatural',
+    type: 'texture',
+    path: '/textures/baked-natural.jpg',
+  },
+  {
+    name: 'bakedTextureNight',
+    type: 'texture',
+    path: '/textures/baked-night.jpg',
+  },
+  {
+    name: 'iphoneTexture',
+    type: 'texture',
+    path: '/textures/iPhone.jpg',
+  },
+  {
+    name: 'project_1',
+    type: 'texture',
+    path: '/textures/project_1.jpg',
+  },
+  {
+    name: 'project_2',
+    type: 'texture',
+    path: '/textures/project_2.jpg',
+  },
+  {
+    name: 'project_3',
+    type: 'texture',
+    path: '/textures/project_3.png',
+  },
+  {
+    name: 'project_4',
+    type: 'texture',
+    path: '/textures/project_4.png',
+  },
+  {
+    name: 'projectDefault',
+    type: 'texture',
+    path: '/textures/project-default.jpg',
+  },
+  {
+    name: 'video',
+    type: 'videoTexture',
+    path: '/textures/video.mp4',
+  },
 ];
